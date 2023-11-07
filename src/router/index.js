095e506bd6d6f2a33146bbc5c8c321b176473237
loren-zhihao <2415612109@qq.com>
2023-11-07 17:39:07 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -407,7 +407,8 @@ export const asyncRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
+  base: "/sps_web/",
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
